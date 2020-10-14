60f592b31a23ffcf779427588da878059a5dc719
Karthik <karthiktalam8@gmail.com>
2020-10-15 02:05:40 +0530

    chat application using pusher is implemented with public channels

--- server/index.ts
+++ server/index.ts
@@ -1,30 +1,66 @@
-import { createServer } from 'http'
-import { parse } from 'url'
+import cors from 'cors'
 import next from 'next'
+import Pusher from 'pusher'
+import express from 'express'
+import bodyParser from 'body-parser'
+require('dotenv').config()
 
-const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
+
 const app = next({ dev })
-const handle = app.getRequestHandler()
+const handler = app.getRequestHandler()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    const { pathname, query } = parsedUrl
-
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(port)
-
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
+// Ensure that your pusher credentials are properly set in the .env file
+// Using the specified variables
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID as string,
+  key: process.env.PUSHER_APP_KEY as string,
+  secret: process.env.PUSHER_APP_SECRET as string,
+  cluster: process.env.PUSHER_APP_CLUSTER as string,
+  encrypted: true
 })
+
+interface Chat {
+  user: any
+  message: any
+  timestamp: any
+}
+
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+
+    server.use(cors())
+    server.use(bodyParser.json())
+    server.use(bodyParser.urlencoded({ extended: true }))
+
+    server.get('*', (req: any, res: any) => {
+      return handler(req, res)
+    })
+
+    const chatHistory: { messages: Chat[] } = { messages: [] }
+
+    server.post('/message', (req: { body: { user?: any; message?: ''; timestamp?: number } }, res) => {
+      const { user, message = '', timestamp = +new Date() } = req.body
+
+      const chat = { user, message, timestamp }
+
+      chatHistory.messages.push(chat)
+      pusher.trigger('chat-room', 'new-message', { chat })
+      res.sendStatus(200)
+    })
+
+    server.post('/messages', (_req, res) => {
+      res.json({ ...chatHistory, status: 'success' })
+    })
+
+    server.listen(port, () => {
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
+  .catch((ex: { stack: any }) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
